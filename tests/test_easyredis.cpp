4c3c4f68b29a540cbfdeae5deae17eb90345dd7a
Chase Holland <chase.holland@lumate.com>
2015-05-04 20:09:47 -0500

    adding test framework

---
+++ tests/test_easyredis.cpp
@@ -0,0 +1,5 @@
+//
+// Created by chase on 5/4/15.
+//
+
+#include "test_easyredis.h"
---
+++ tests/test_easyredis.h
@@ -0,0 +1,8 @@
+//
+// Created by chase on 5/4/15.
+//
+
+#ifndef EASYREDIS_TEST_EASYREDIS_H
+#define EASYREDIS_TEST_EASYREDIS_H
+
+#endif //EASYREDIS_TEST_EASYREDIS_H
