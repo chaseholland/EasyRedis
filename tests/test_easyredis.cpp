3af7354ad36e5697cfaac76a7d3d9978b7ea44bb
Chase Holland <chase.holland@lumate.com>
2015-05-04 21:06:12 -0500

    adding a tesT

--- tests/test_easyredis.cpp
+++ tests/test_easyredis.cpp
@@ -3,3 +3,17 @@
 //
 
 #include "test_easyredis.h"
+#include "gtest/gtest.h"
+#include "../EasyRedis.h"
+
+TEST (EasyRedisTest, TestContextCreation)
+{
+    EasyRedis easyRedis = EasyRedis("localhost", 6379);
+    EXPECT_TRUE(easyRedis.raw_redis_context() != NULL);
+    ASSERT_EQ(easyRedis.raw_redis_context()->err, 0);
+}
+
+TEST (EasyRedisTest, TestCommand)
+{
+    EasyRedis easyRedis = EasyRedis("localhost", 6379);
+}
